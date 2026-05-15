b5c7e39646fb83113ed5f4ae79815a99b675a0c9
Edson5Leonard <edsonleonardorojascabia@gmail.com>
2026-05-15 15:14:35 -0500

    Subir modulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
-import { AttendanceModule } from './modules/attendance/attendance.module';
-import { AsistenciaModule } from './modules/asistencia/asistencia.module';
-import { CommisionModule } from './modules/commision/commision.module';
-import { ComicionModule } from './modules/comicion/comicion.module';
-import { ComisionModule } from './modules/comision/comision.module';
-import { JustificcacionModule } from './modules/justificcacion/justificcacion.module';
+
 import { JustificacionModule } from './modules/justificacion/justificacion.module';
 import { TareasModule } from './modules/tareas/tareas.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, AttendanceModule, AsistenciaModule, CommisionModule, ComicionModule, ComisionModule, JustificcacionModule, JustificacionModule, TareasModule],
+  imports: [AuthModule, UsersModule, JustificacionModule, TareasModule],
   controllers: [AppController],
   providers: [AppService],
 })
